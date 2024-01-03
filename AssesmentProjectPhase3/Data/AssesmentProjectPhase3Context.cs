d5837db912e11cc6c725d26f2ff80f530397120e
2901shivam <shivamsingh201617@gmail.com>
2024-01-03 14:59:09 +0530

    AssesmentProject

---
+++ AssesmentProjectPhase3/Data/AssesmentProjectPhase3Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AssesmentProjectPhase3.Models;
+
+namespace AssesmentProjectPhase3.Data
+{
+    public class AssesmentProjectPhase3Context : DbContext
+    {
+        public AssesmentProjectPhase3Context (DbContextOptions<AssesmentProjectPhase3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AssesmentProjectPhase3.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<AssesmentProjectPhase3.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
